563f9934c8a664886bef84c0f4bf525d9d062852
bewuwy <bewuwy@gmail.com>
2020-10-10 23:37:13 +0200

    added a system that makes a temporary text channel

--- yiay-us-dc.py
+++ yiay-us-dc.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 from os import environ
 import random
 import json
+import time
 
 client = commands.Bot(command_prefix=">")
 token = environ.get("token")
@@ -35,20 +36,18 @@ async def start(ctx):
         embed.add_field(name="You are not in a vc!",
                         value="You can't play without being connected to a vc! You need to connect to a vc to play.")
         await channel.send(embed=embed)
-
         return
 
     players = vc.members
-
-    await guild.change_voice_state(channel=vc, self_deaf=True, self_mute=True)
 
     if len(players) < 2:
         embed = discord.Embed(title="Error", color=discord.Colour.red())
         embed.add_field(name="To little users!",
                         value="There are too little users in the voice channel! You need at least 3 people to play.")
         await channel.send(embed=embed)
-
         return
+
+    await guild.change_voice_state(channel=vc, self_deaf=True, self_mute=True)
 
     crewmates = list(players)
     impostors = [players[random.randint(0, len(players)-1)]]
@@ -72,14 +71,53 @@ async def start(ctx):
     i_q = questions["impostor"][n]
 
     for i in crewmates:
-        await i.create_dm()
-        await i.dm_channel.send("You are a Crewmate!")
-        await i.dm_channel.send(c_q)
+        m = guild.get_member(i.id)
+
+        if m.dm_channel is None:
+            await m.create_dm()
+
+        await m.dm_channel.send("You are a Crewmate!")
+        await m.dm_channel.send(c_q)
 
     for i in impostors:
-        await i.create_dm()
-        await i.dm_channel.send("You are The Impostor!")
-        await i.dm_channel.send(i_q)
+        m = guild.get_member(i.id)
+
+        if m.dm_channel is None:
+            await m.create_dm()
+
+        await m.dm_channel.send("You are The Impostor!")
+        await m.dm_channel.send(i_q)
+
+    chan_name = f"yiay-us-{random.randint(1000, 9999)}"
+    game_role = await guild.create_role(name=chan_name)
+    print(f"Starting {chan_name} game")
+
+    overwrites = {
+        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        game_role: discord.PermissionOverwrite(read_messages=True),
+        guild.me: discord.PermissionOverwrite(read_messages=True)
+    }
+    game_channel = await guild.create_text_channel(chan_name, overwrites=overwrites)
+
+    for i in players:
+        await i.add_roles(game_role)
+
+    for i in players:
+        temp_msg = await game_channel.send(f"{i.mention} it's your turn! You have 60 seconds to answer.")
+
+        def answer_check(msg):
+            return msg.channel == game_channel and msg.author == i
+
+        message = await client.wait_for('message', check=answer_check, timeout=60)
+        await message.add_reaction("✅")
+        await temp_msg.delete()
+
+    time.sleep(20)
+    await guild.change_voice_state(channel=None)
+    for i in players:
+        await i.remove_roles(game_role)
+    await game_channel.delete()
+    await game_role.delete()
 
 
 client.add_command(start)
