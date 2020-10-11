78ce195dd2f3a464b602d7ea8a9115ae3c22daf8
bewuwy <bewuwy@gmail.com>
2020-10-11 09:57:19 +0200

    Added voting mechanic
    (and some TODOs) also now the bot wont delete the  game channel

--- yiay-us-dc.py
+++ yiay-us-dc.py
@@ -18,6 +18,7 @@ async def on_ready():
 @commands.command()
 async def start(ctx):
 
+    # checking some things
     user = ctx.author
     guild = ctx.guild
     channel = ctx.channel
@@ -49,6 +50,7 @@ async def start(ctx):
 
     await guild.change_voice_state(channel=vc, self_deaf=True, self_mute=True)
 
+    # selecting impostor(s) TODO: add settings so you can select number of impostors
     crewmates = list(players)
     impostors = [players[random.randint(0, len(players)-1)]]
 
@@ -57,6 +59,8 @@ async def start(ctx):
 
     # print(f"crew: {crewmates}")
     # print(f"impostors: {impostors}")
+
+    # selecting a question TODO: make some example questions
     questions = json.load(open("questions.json"))
 
     q_number = len(questions["crewmate"])
@@ -70,6 +74,7 @@ async def start(ctx):
     c_q = questions["crewmate"][n]
     i_q = questions["impostor"][n]
 
+    # announcing roles TODO: make it pretty with embed messages with images
     for i in crewmates:
         m = guild.get_member(i.id)
 
@@ -88,12 +93,13 @@ async def start(ctx):
         await m.dm_channel.send("You are The Impostor!")
         await m.dm_channel.send(i_q)
 
+    # creating the game channel
     chan_name = f"yiay-us-{random.randint(1000, 9999)}"
-    game_role = await guild.create_role(name=chan_name)
+    game_role = await guild.create_role(name=chan_name)  # TODO: change it to specific users rather than a role
     print(f"Starting {chan_name} game")
 
     overwrites = {
-        guild.default_role: discord.PermissionOverwrite(read_messages=False),
+        guild.default_role: discord.PermissionOverwrite(read_messages=False, add_reactions=False),
         game_role: discord.PermissionOverwrite(read_messages=True),
         guild.me: discord.PermissionOverwrite(read_messages=True)
     }
@@ -102,6 +108,8 @@ async def start(ctx):
     for i in players:
         await i.add_roles(game_role)
 
+    # answering phase TODO: maybe change it to dms so you cant change your mind based on other answers
+    messages = []
     for i in players:
         temp_msg = await game_channel.send(f"{i.mention} it's your turn! You have 60 seconds to answer.")
 
@@ -109,15 +117,62 @@ async def start(ctx):
             return msg.channel == game_channel and msg.author == i
 
         message = await client.wait_for('message', check=answer_check, timeout=60)
-        await message.add_reaction("✅")
+        messages.append(message)
         await temp_msg.delete()
 
+    # voting phase TODO: say who is voted out and overwrite so dead people can't talk
+    await game_channel.send(f"The question was: {c_q}")
+    vote_announce = await game_channel.send(f"Voting starts in 20 seconds {game_role.mention}")
     time.sleep(20)
+    await vote_announce.delete()
+
+    not_voted = list(players)
+    votes = {}
+
+    skip_msg = await game_channel.send("Skip vote")
+    messages.append(skip_msg)
+    for i in messages:
+        await i.add_reaction("✅")
+    await game_channel.send(f"You can now vote! Remember that you can't change your vote!")
+
+    def vote_check(reaction, user):
+        return not user.bot
+
+    while not_voted:
+        reaction, user = await client.wait_for("reaction_add", check=vote_check, timeout=90)
+
+        if reaction.message.channel == game_channel and str(reaction.emoji) == "✅" and user in not_voted:
+            not_voted.remove(user)
+
+            if reaction.message.author in votes.keys():
+                votes[reaction.message.author] += 1
+            else:
+                votes[reaction.message.author] = 1
+
+            await game_channel.send(f"{user.mention} has voted!")
+            await reaction.remove(user)
+
+        else:
+            await reaction.delete()
+
+    # voting results TODO: add list who voted for what
+    await game_channel.send("Voting results:")
+    for i in votes.keys():
+        if i != client.user:
+            await game_channel.send(f"{i.mention} got {votes[i]} votes")
+        else:
+            await game_channel.send(f"Skipped: {votes[i]}")
+
+    # TODO: make game longer than one round
+    time.sleep(30)
+    # cleaning after the end of the game
+    print(f"Cleaning {chan_name} game")
     await guild.change_voice_state(channel=None)
     for i in players:
         await i.remove_roles(game_role)
-    await game_channel.delete()
     await game_role.delete()
+
+    # await game_channel.delete() TODO: make it so you need to confirm to delete the channel
 
 
 client.add_command(start)
